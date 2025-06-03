9f6062b4e38490f95e5d6961390f03b5a5c7f64d
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-03 16:49:00 -0300

    Aula 41: Projeto Classificação (processando embeddings)

---
+++ nearest_neighbors/process_embeddings.js
@@ -0,0 +1,18 @@
+import fs, { writeFileSync } from "fs"
+
+const files = fs.readdirSync("./embeddings").map(f => "./embeddings/" + f);
+
+let embeddings = []
+
+for (let file of files){
+    const fileJSON = JSON.parse(fs.readFileSync(file));
+    embeddings = embeddings.concat(fileJSON);
+}
+
+for (let embedding of embeddings) {
+    embedding["number"] = parseInt(/\d+/.exec(embedding["path"]));
+    embedding["split"] = embedding["number"] < 500 ? "test" : "train";
+    embedding["class"] = embedding["path"].includes("cat") ? "cat" : "dog";
+}
+
+fs.writeFileSync("./embeddings.json", JSON.stringify(embeddings));
