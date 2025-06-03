60558a533b25a1331ee4d3132157078a728c75da
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-03 17:25:32 -0300

    Aula 43: Aplicando TSNE em embeddings

--- tsne/tsne.js
+++ tsne/tsne.js
@@ -1,7 +1,16 @@
 import TSNE from "tsne-js";
 import fs from "fs";
+import { createObjectCsvWriter } from "csv-writer";
 
 let embeddings = JSON.parse(fs.readFileSync("../nearest_neighbors/embeddings.json"));
+
+let tsneInput = [];
+
+for (let embedding of embeddings){
+    if(embedding["number"] >= 500 && embedding["number"] <= 1000){
+        tsneInput.push(embedding);
+    }
+}
 
 let model = new TSNE({
     dim: 2,
@@ -13,12 +22,30 @@ let model = new TSNE({
 });
 
 model.init({
-    data: [],
+    data: tsneInput.map(i => i["embedding"]),
     type: "dense"
 });
 
 model.run();
 
 let output = model.getOutput();
+let csvOutput = [];
 
-console.log(output);
+for (let i=0; i<tsneInput.length; i++){
+    csvOutput.push({
+        class: tsneInput[i]["class"],
+        x: output[i][0],
+        y: output[i][1]
+    })
+}
+
+const csvWriter = createObjectCsvWriter({
+    path: "tsne_output.csv",
+    header: [
+        {id: "class", title: "color"},
+        {id: "x", title: "x"},
+        {id: "y", title: "y"},
+    ]
+})
+
+await csvWriter.writeRecords(csvOutput);
