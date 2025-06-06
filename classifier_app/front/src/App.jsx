81256fe779f255301b408583bca9392af3e4a429
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-06 13:54:54 -0300

    Aula 59: Ajustando o Frontend

--- classifier_app/front/src/App.jsx
+++ classifier_app/front/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+function CaptionedImage({ imgSrc, method }){
+  const [category, setCategory] = useState(null);
+
+  useEffect(() => {
+    const getCaption = async () => {
+      const response = await fetch("http://127.0.0.1:3000/classify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({method: method, imgPath: imgSrc})
+      }).then(resp => resp.json());
+
+      setCategory(response["category"]);
+    };
+
+    getCaption();
+  }, []);
+
+  return <>
+    <div style={{display: "flex", flexDirection: "column"}}>
+        { category === null ? <h2>It's a ...</h2> : <h2>It's a {category}!!!</h2>}
+        <img src={imgSrc} style={{width: "100%", marginTop: "20px", borderRadius: 10}} />
+        
+      </div>
+  </>
+}
 
 function App() {
 const [textInput, setTextInput] = useState(null);
 const [imgSrc, setImgSrc] = useState(null);
+const [method, setMethod] = useState("llm")
 
-async function classify(){
-  console.log(textInput);
+function classify(){
+  setImgSrc(null);
   setImgSrc(textInput);
-
-  const response = await fetch("http://127.0.0.1:3000/classify", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({method: "knn", imgPath: textInput})
-  });
-
-  console.log(await response.json());
 }
 
   return (
     <>
       <h1>Cat or Dog?</h1>
       <input onChange={e => (setTextInput(e.target.value))} style={{width: "100%"}} />
-      <button onClick={classify}>Classify!</button>
-      <div style={{display: "flex", flexDirection: "column"}}>
-        <img src={imgSrc} style={{height: "200px", marginTop: "20px"}} />
-        <span>Caption</span>
-      </div>
+      Method: <select onChange={e => setMethod(e.target.value)}>
+        <option value="llm">LLM</option>
+        <option value="knn">KNN</option>
+      </select>
+      <button onClick={classify} style={{margin: 10}}>Classify!</button>
+      {
+        imgSrc !== null ? <CaptionedImage imgSrc={imgSrc} method={method}></CaptionedImage> : null
+      }
     </>
   )
 }
