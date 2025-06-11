0fb5c11c6a9a45a0ca700f39eaac6a4010430c8f
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-11 17:18:48 -0300

    Aula 12: Gemini API

--- semantic_search/embeddings_example/embeddings.js
+++ semantic_search/embeddings_example/embeddings.js
@@ -1,4 +1,5 @@
-import { pipeline, cos_sim } from "@huggingface/transformers"
+import { pipeline, cos_sim } from "@huggingface/transformers";
+import { GoogleGenAI } from "@google/genai";
 
 const embedder = await pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2", { dtype: "q8" })
 const imgEmbedder = await pipeline("image-feature-extraction", "Xenova/clip-vit-base-patch32", { dtype: "q8" })
@@ -11,18 +12,15 @@ async function embedImg(img) {
   return imgEmbedder(img, { pooling: "cls", normalize: true }).then(t => t.tolist()[0]);
 }
 
-const babyHippo = "https://i.ytimg.com/vi/96xC5JIkIpQ/maxresdefault.jpg"
-const babyHippo2 = "https://i.natgeofe.com/n/74c229e3-4ff7-49d5-bcfd-75e0033816f0/NationalGeographic_1148167.jpg"
 
-const cat = "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_16x9.jpg?w=1200"
-const cat2 = "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg"
+const genai = new GoogleGenAI({apiKey: process.env.GOOGLE_GEN_AI_API_KEY });
 
-const train = "https://www.explore.com/img/gallery/the-oldest-vintage-trains-around-the-world-you-can-still-ride-today/intro-1725233481.jpg"
-const car = "https://www.topgear.com/sites/default/files/2024/11/Original-25901-aw609563.jpg"
+async function embedWithGoogle(text) {
+    return genai.models.embedContent({
+    model: "models/text-embedding-004",
+    contents: [text]
+    }).then(r => r.embeddings[0].values)
+}
 
-console.log(cos_sim(await embedImg(babyHippo), await embedImg(babyHippo2)));
-console.log(cos_sim(await embedImg(cat), await embedImg(cat2)));
-console.log(cos_sim(await embedImg(babyHippo), await embedImg(train)));
-console.log(cos_sim(await embedImg(car), await embedImg(train)));
-
-
+console.log(cos_sim((await embedWithGoogle("olá, tudo bem?")), await embedWithGoogle("oi, meu nome é arthur")))
+console.log(cos_sim((await embedWithGoogle("olá, tudo bem?")), await embedWithGoogle("eu gosto de pizza")))
