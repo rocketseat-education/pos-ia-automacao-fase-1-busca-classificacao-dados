4115b82d82d7c35ce650ee979f91f79528b46b19
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-11 16:35:22 -0300

    Aula 9: Pooling e Normalization

--- semantic_search/embeddings_example/embeddings.js
+++ semantic_search/embeddings_example/embeddings.js
@@ -1,5 +1,5 @@
-import { pipeline, cos_sim } from "@huggingface/transformers";
+import { pipeline } from "@huggingface/transformers"
 
 const embedder = await pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2", { dtype: "q8" })
 
-console.log(await embedder("oi tudo bem?"))
+console.log(await embedder("oi tudo bem? meu nome é arthur", { pooling: "mean", normalize: true }))
