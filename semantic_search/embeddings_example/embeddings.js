fe6ef0730f18add931d88ca102abd3e3fadd6cae
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-11 17:15:02 -0300

    Aula 11: Comparando imagens

--- semantic_search/embeddings_example/embeddings.js
+++ semantic_search/embeddings_example/embeddings.js
@@ -1,21 +1,28 @@
 import { pipeline, cos_sim } from "@huggingface/transformers"
 
 const embedder = await pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2", { dtype: "q8" })
+const imgEmbedder = await pipeline("image-feature-extraction", "Xenova/clip-vit-base-patch32", { dtype: "q8" })
 
 async function embedText(text) {
   return embedder(text, { pooling: "mean", normalize: true }).then(t => t.tolist()[0]);
 }
 
-console.log(cos_sim(await embedText("king"), await embedText("queen")))
-console.log(cos_sim(await embedText("king"), await embedText("woman")))
-console.log(cos_sim(await embedText("king"), await embedText("man")))
-console.log(cos_sim(await embedText("i drove my car to work"), await embedText("i walked home from work")))
-console.log(cos_sim(await embedText("i drove my car to work"), await embedText("i ate pizza at home for dinner")))
-console.log(cos_sim(await embedText("i drank juice for lunch"), await embedText("i ate pizza at home for dinner")))
+async function embedImg(img) {
+  return imgEmbedder(img, { pooling: "cls", normalize: true }).then(t => t.tolist()[0]);
+}
 
-console.log(cos_sim(await embedText("France"), await embedText("Italy")))
-console.log(cos_sim(await embedText("France"), await embedText("China")))
+const babyHippo = "https://i.ytimg.com/vi/96xC5JIkIpQ/maxresdefault.jpg"
+const babyHippo2 = "https://i.natgeofe.com/n/74c229e3-4ff7-49d5-bcfd-75e0033816f0/NationalGeographic_1148167.jpg"
 
-console.log(cos_sim(await embedText("France"), await embedText("Pizza")))
-console.log(cos_sim(await embedText("Italy"), await embedText("Pizza")))
+const cat = "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_16x9.jpg?w=1200"
+const cat2 = "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg"
+
+const train = "https://www.explore.com/img/gallery/the-oldest-vintage-trains-around-the-world-you-can-still-ride-today/intro-1725233481.jpg"
+const car = "https://www.topgear.com/sites/default/files/2024/11/Original-25901-aw609563.jpg"
+
+console.log(cos_sim(await embedImg(babyHippo), await embedImg(babyHippo2)));
+console.log(cos_sim(await embedImg(cat), await embedImg(cat2)));
+console.log(cos_sim(await embedImg(babyHippo), await embedImg(train)));
+console.log(cos_sim(await embedImg(car), await embedImg(train)));
+
 
