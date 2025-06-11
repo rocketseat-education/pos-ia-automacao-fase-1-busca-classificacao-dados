3c269358e3933f566cc964a9b665af26c1b37483
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-11 17:10:22 -0300

    Aula 10: Comparando palavras

--- semantic_search/embeddings_example/embeddings.js
+++ semantic_search/embeddings_example/embeddings.js
@@ -1,5 +1,21 @@
-import { pipeline } from "@huggingface/transformers"
+import { pipeline, cos_sim } from "@huggingface/transformers"
 
 const embedder = await pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2", { dtype: "q8" })
 
-console.log(await embedder("oi tudo bem? meu nome é arthur", { pooling: "mean", normalize: true }))
+async function embedText(text) {
+  return embedder(text, { pooling: "mean", normalize: true }).then(t => t.tolist()[0]);
+}
+
+console.log(cos_sim(await embedText("king"), await embedText("queen")))
+console.log(cos_sim(await embedText("king"), await embedText("woman")))
+console.log(cos_sim(await embedText("king"), await embedText("man")))
+console.log(cos_sim(await embedText("i drove my car to work"), await embedText("i walked home from work")))
+console.log(cos_sim(await embedText("i drove my car to work"), await embedText("i ate pizza at home for dinner")))
+console.log(cos_sim(await embedText("i drank juice for lunch"), await embedText("i ate pizza at home for dinner")))
+
+console.log(cos_sim(await embedText("France"), await embedText("Italy")))
+console.log(cos_sim(await embedText("France"), await embedText("China")))
+
+console.log(cos_sim(await embedText("France"), await embedText("Pizza")))
+console.log(cos_sim(await embedText("Italy"), await embedText("Pizza")))
+
