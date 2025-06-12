34aa5747e1571a8ccb8701a45c6e0492f1ffc5ad
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-06-12 14:49:18 -0300

    Aula 22: Lendo dados do arquivo CSV

--- semantic_search/semantic_search_example/semantic_search_project/data_loader/load_data.js
+++ semantic_search/semantic_search_example/semantic_search_project/data_loader/load_data.js
@@ -1,11 +1,28 @@
 import { ChromaClient } from "chromadb";
+import csv from "csv-parser";
+import fs from "fs";
 
 const chromaClient = new ChromaClient();
-const collection = await chromaClient.getOrCreateCollection({ name: "test" });
+const collection = await chromaClient.getOrCreateCollection({ name: "movies" });
 
-await collection.add({
-    ids: ["1", "2", "3"],
-    documents: ["olá", "oi", "tudo bom"],
-    metadatas: [{"title": "meu filme"}, {"nome": "arthur"}, {}]
-});
+const ids = [];
+const documents = [];
+const metadatas = [];
+
+fs.createReadStream("mpst_full_data.csv")
+.pipe(csv())
+.on('data', (row) => {
+    const document = {"title": row["title"], "tags": row["tags"], "synopsis": row["plot_synopsis"]};
+
+    ids.push(row["imdb_id"]);
+    documents.push(JSON.stringify(document));
+    metadatas.push(document);
+})
+.on('end', async () => {
+    await collection.add({
+    ids: ids,
+    documents: documents,
+    metadatas: metadatas,
+    });
+})
 
