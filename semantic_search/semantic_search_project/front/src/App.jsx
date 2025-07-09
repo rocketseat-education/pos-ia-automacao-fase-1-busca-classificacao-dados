78e0998dcd729b2a32707da4077836c64e30d957
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-07-09 16:41:16 -0300

    Aula 11: Consertando estilo

--- semantic_search/semantic_search_project/front/src/App.jsx
+++ semantic_search/semantic_search_project/front/src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { ChromaClient } from "chromadb";
-import { useEffect } from 'react';
 import { env } from "chromadb-default-embed";
 
 env.useBrowserCache = false;
 env.allowLocalModels = false;
 
-function MovieTag({ tag }){
-  return <div style={{border: "1px solid grey", padding: 5, fontSize: 12, borderRadius: 7}}>{tag}</div>
+function MovieTag({ tag }) {
+  return <div className="movie-tag">{tag}</div>
 }
 
-function MovieCard({ title, tags, synopsis }){
-  return <div className="movie-card" style={{display: "flex", flexDirection: "column", gap: 5, border: "1px solid grey", borderRadius: 10, padding: "10px", height: "250px"}}>
-    <h2 style={{margin: 0}}>{title}</h2>
-    <div style={{display: "flex", justifyContent: "center", gap: 5}}>{tags.map((t, idx) => <Movietag key={idx} tag={t}/>)}</div>
-    <div style={{overflow: "auto", padding: 5}}>{synopsis}</div>
-  </div>
+function MovieCard({ title, tags, synopsis }) {
+  return (
+    <div className="movie-card">
+      <h2 className="movie-title">{title}</h2>
+      <div className="tag-container">
+        <div className="tag-holder">
+          {tags.map((t, idx) => <MovieTag key={idx} tag={t} />)}
+        </div>
+      </div>
+      <div className="synopsis">{synopsis}</div>
+    </div>
+  )
 }
 
 function App() {
@@ -26,19 +31,18 @@ function App() {
   const [isConnected, setIsConnected] = useState(false);
   const [queryText, setQueryText] = useState("");
 
-
-  async function queryDatabase(){
+  async function queryDatabase() {
     setIsLoading(true);
-    const res = await chromaCollection.query({queryTexts: queryText});
+    const res = await chromaCollection.query({ queryTexts: queryText });
     const movies = [];
 
-    for(let i = 0; i < res.ids[0].length; i++){
+    for (let i = 0; i < res.ids[0].length; i++) {
       movies.push({
-          id: res.ids[0][i],
-          distance: res.distances[0][i],
-          title: res.metadatas[0][i].title,
-          tags: res.metadatas[0][i].tags.split(", "),
-          synopsis: res.metadatas[0][i].synopsis,
+        id: res.ids[0][i],
+        distance: res.distances[0][i],
+        title: res.metadatas[0][i].title,
+        tags: res.metadatas[0][i].tags.split(", "),
+        synopsis: res.metadatas[0][i].synopsis,
       });
     }
 
@@ -47,30 +51,28 @@ function App() {
   }
 
   useEffect(() => {
-
     const initializeChroma = async () => {
       const chromaClient = new ChromaClient();
-      const collection = await chromaClient.getOrCreateCollection({name: "movies"});
+      const collection = await chromaClient.getOrCreateCollection({ name: "movies" });
 
       setChromaCollection(collection);
       setIsConnected(true);
     }
-    
+
     initializeChroma();
   }, []);
 
   return (
     <>
       <h1>Movie Recommender</h1>
-      <div style={{display: "flex", flexDirection: "column"}}>
+      <div className="form-container">
         <textarea onChange={e => setQueryText(e.target.value)}></textarea>
         <button onClick={queryDatabase} disabled={!isConnected || isLoading}>Submit</button>
       </div>
       {isLoading ? "Loading..." : null}
-      <div className="movie-list" style={{marginTop: "15px", display: "flex", flexDirection: "column", gap: 5}}>
+      <div className="movie-list">
         {movieList.map((m, idx) => <MovieCard key={idx} {...m} />)}
       </div>
-
     </>
   )
 }
